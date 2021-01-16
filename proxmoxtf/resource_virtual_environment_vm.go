dbaad88ea656c5353827bbba1356606c3a51370d
Nikita Khomyakov <dr.evil@krtn.eu>
2021-01-16 00:17:15 +0000

    Fix VM updates with an empty name

--- proxmoxtf/resource_virtual_environment_vm.go
+++ proxmoxtf/resource_virtual_environment_vm.go
@@ -3017,7 +3017,11 @@ func resourceVirtualEnvironmentVMUpdate(d *schema.ResourceData, m interface{}) e
 	}
 
 	name := d.Get(mkResourceVirtualEnvironmentVMName).(string)
-	updateBody.Name = &name
+	if name == "" {
+		delete = append(delete, "name")
+	} else {
+		updateBody.Name = &name
+	}
 
 	if d.HasChange(mkResourceVirtualEnvironmentVMTabletDevice) {
 		tabletDevice := proxmox.CustomBool(d.Get(mkResourceVirtualEnvironmentVMTabletDevice).(bool))
